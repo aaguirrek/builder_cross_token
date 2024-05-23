a46e5d13d588086c2de61b1758be9b237fb7b365
Alejandro <aaguirrek@ucsm.edu.pe>
2024-05-23 16:41:48 -0500

    dsa

--- builder_cross/www/page2.py
+++ builder_cross/www/page2.py
@@ -13,18 +13,27 @@ def get_context(context, **dict_params):
 	frappe.local.form_dict.update(dict_params)
 	name = frappe.local.form_dict.name
 	variable = frappe.local.form_dict.variable
-	
+	websetting = frappe.get_doc("Website Settings", "Website Settings")
+
 	if variable != None:
 		docname = frappe.get_list(doctype='Buildhub Web',fields=["name"],filters=[["route","=","/"+name],["es_dinamica","=",1]])
 		if len(docname)==0:
 			docname = frappe.get_list(doctype='Buildhub Web',fields=["name"],filters=[["route","=",name],["es_dinamica","=",1]])
-		
-		doc = frappe.get_doc("Buildhub Web",docname[0].name)
+		if len(docname)>0:
+			doc = frappe.get_doc("Buildhub Web",docname[0].name)
 	else:
 		docname = frappe.get_list(doctype='Buildhub Web',fields=["name"],filters=[["route","=","/"+name]])
 		if len(docname)==0:
 			docname = frappe.get_list(doctype='Buildhub Web',fields=["name"],filters=[["route","=",name]])
-		doc = frappe.get_doc("Buildhub Web",docname[0].name)
+		if len(docname)>0:
+			doc = frappe.get_doc("Buildhub Web",docname[0].name)
+	
+	if len(docname)==0:
+		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
 		
+	if doc.log_in==1:
+		if frappe.session.user == "Guest":
+			frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
 	context.doc = doc
 	context.user = frappe.get_user()
+	context.settings = websetting
